9a746d06e6a29786aa96ca33b35e851879e209b8
Ершов Егор <ershegm@gmail.com>
2024-04-07 00:05:45 +0500

    Подключение моделей

--- src/main.js
+++ src/main.js
@@ -9,8 +9,20 @@ import PointsPresenter from './presenter/board-presenter.js';
 const tripMainContainer = document.querySelector('.trip-main');
 const tripEventsContainer = document.querySelector('.trip-events');
 const filtersContainer = tripMainContainer.querySelector('.trip-controls__filters');
+
+import PointsModel from './model/event-points-model.js';
+import OffersModel from './model/offers-model.js';
+import DestinationsModel from './model/destinations-model.js';
+
+const pointsModel = new PointsModel();
+const offersModel = new OffersModel();
+const destinationsModel = new DestinationsModel();
+
 const pointsPresenter = new PointsPresenter({
-  tripEventsContainer
+  tripEventsContainer,
+  pointsModel,
+  offersModel,
+  destinationsModel
 });
 
 render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
