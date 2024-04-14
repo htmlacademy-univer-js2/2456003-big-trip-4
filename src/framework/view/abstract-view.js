eac4033f69d0ef2e4ffcf68607f45bb83fff85f5
Ершов Егор <ershegm@gmail.com>
2024-04-14 20:49:56 +0500

    test4 fix merge pull requests

--- src/framework/view/abstract-view.js
+++ src/framework/view/abstract-view.js
@@ -1,31 +1,68 @@
-import {
-  render,
-  RenderPosition
-} from './render.js';
-import TripInfoView from './view/create-trip.js';
-import FiltersView from './view/create-filter.js';
-import PointsPresenter from './presenter/board-presenter.js';
+import {createElement} from '../render.js';
+import './abstract-view.css';
 
-const tripMainContainer = document.querySelector('.trip-main');
-const tripEventsContainer = document.querySelector('.trip-events');
-const filtersContainer = tripMainContainer.querySelector('.trip-controls__filters');
+/** @const {string} Класс, реализующий эффект "покачивания головой" */
+const SHAKE_CLASS_NAME = 'shake';
 
-import PointsModel from './model/event-points-model.js';
-import OffersModel from './model/offers-model.js';
-import DestinationsModel from './model/destinations-model.js';
+/** @const {number} Время анимации в миллисекундах */
+const SHAKE_ANIMATION_TIMEOUT = 600;
 
-const pointsModel = new PointsModel();
-const offersModel = new OffersModel();
-const destinationsModel = new DestinationsModel();
+/**
+ * Абстрактный класс представления
+ */
+export default class AbstractView {
+  /** @type {HTMLElement|null} Элемент представления */
+  #element = null;
 
-const pointsPresenter = new PointsPresenter({
-  tripEventsContainer,
-  pointsModel,
-  offersModel,
-  destinationsModel
-});
+  /** @type {Object} Объект с колбэками. Может использоваться для хранения обработчиков событий */
+  _callback = {};
 
-render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
-render(new FiltersView(), filtersContainer);
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Can\'t instantiate AbstractView, only concrete one.');
+    }
+  }
 
-pointsPresenter.init();
+  /**
+   * Геттер для получения элемента
+   * @returns {HTMLElement} Элемент представления
+   */
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  /**
+   * Геттер для получения разметки элемента
+   * @abstract
+   * @returns {string} Разметка элемента в виде строки
+   */
+  get template() {
+    throw new Error('Abstract method not implemented: get template');
+  }
+
+  /** Метод для удаления элемента */
+  removeElement() {
+    this.#element = null;
+  }
+
+  /**
+   * Метод, реализующий эффект "покачивания головой"
+   * @param {shakeCallback} [callback] Функция, которая будет вызвана после завершения анимации
+   */
+  shake(callback) {
+    this.element.classList.add(SHAKE_CLASS_NAME);
+    setTimeout(() => {
+      this.element.classList.remove(SHAKE_CLASS_NAME);
+      callback?.();
+    }, SHAKE_ANIMATION_TIMEOUT);
+  }
+}
+
+/**
+ * Функция, которая будет вызвана методом shake после завершения анимации
+ * @callback shakeCallback
+ */
